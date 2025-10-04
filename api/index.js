ecb6e4cc86fa9df62f4961d1b8ffc59d093db6cc
PKachariya <parthkachariya9727@gmail.com>
2025-10-04 17:16:43 +0530

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from 'cors';
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -11,9 +13,23 @@ mongoose.connect(process.env.MONGO).then(() => {
 });
 
 const app = express();
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+// Simple request logger to help debug incoming requests
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path}`);
+    next();
+});
+
+// Mount routes
+app.use("/api/user", userRouter);
+// Auth routes should be under /api/auth (not /api/user)
+app.use("/api/auth", authRouter);
+
+// Start server after routes are mounted
 app.listen(3000, () => {
     console.log('server is running on port 3000');
 });
-
-app.use("/api/user", userRouter);
